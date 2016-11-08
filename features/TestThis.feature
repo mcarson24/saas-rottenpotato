14315e7247058e7b9388a21db5ccc4df63c65e6b
Matthew Carson <matthew24carson@gmail.com>
2016-11-08 11:36:23 -0500

    Add behat for BDD

---
+++ features/TestThis.feature
@@ -0,0 +1,8 @@
+Feature: Test this feature
+  As an annoyed developer
+  So that I don't throw my computer at the wall
+  I want to make sure behat is working
+
+Scenario: I try to test this out
+  Given I am on the homepage
+  Then I should see "All Movies"
---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,26 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
+use Laracasts\Behat\Context\Migrator;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext extends MinkContext implements Context
+{
+	use Migrator;
+
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+}
